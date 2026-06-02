f241fb5236e0f17ceb8901511117161ecc33a58b
Mridulsharma1133 <mridulsharma1133@gmail.com>
2026-06-02 23:51:17 +0530

    "Longest Substring problem without repeating characters"

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
-nums = [5]
-k = 1
-Sum = 0
-maxAvg = 0
 
-if (len(nums) == 1):
-    print(nums[0]/k)
+s = "au"
 
-for i in range(k):
-    Sum += nums[i]
 
-avg = Sum/k
-maxAvg = max(maxAvg, avg)
-i = 0
-for j in range(k, len(nums)):
-    Sum += nums[j] - nums[j - k]
-    avg = Sum / k
-    maxAvg = max(maxAvg, avg)
-print(maxAvg)
+unique = s[0] 
+subStr = s[0] 
+maxLen = len(subStr)
+best = s[0]
+for j in range(1, len(s)):
+    if s[j] in unique:
+        if len(subStr) > maxLen:
+            maxLen = len(subStr)
+            best = subStr
+        idx = unique.index(s[j])
+        unique = unique[idx +1 :] + s[j]
+        subStr = unique
+    else:
+        subStr += s[j]
+        unique += s[j]
     
+    if len(subStr) > maxLen:
+        maxLen = len(subStr)
+        best = subStr
+
+
+print(maxLen)
+
+
+
