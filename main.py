8c522cea47c24e9ad042e1579649812d499db062
Mridulsharma1133 <mridulsharma1133@gmail.com>
2026-06-04 16:10:21 +0530

    Reverse vowels of String

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
-from PIL import Image
-import os
+s = "leetcode"
 
-input_folder = "images"      # Folder containing PNG files
-output_folder = "webP"    # Folder for WebP files
+print(s[3].lower())
 
-# Create output folder if it doesn't exist
-os.makedirs(output_folder, exist_ok=True)
 
-for filename in os.listdir(input_folder):
-    if filename.lower().endswith(".png"):
-        png_path = os.path.join(input_folder, filename)
 
-        # Remove .png extension and add .webp
-        webp_filename = os.path.splitext(filename)[0] + ".webp"
-        webp_path = os.path.join(output_folder, webp_filename)
+def reverseVowels(s):
+    lst_str = list(s)
+    left  = 0
+    vowels = "aeiou"
+    right = len(s) - 1
+    while(left < right):
+        if s[left].lower() in vowels and s[right].lower() in vowels:
+            lst_str[left], lst_str[right] = lst_str[right], lst_str[left]
+            left += 1
+            right -= 1
+        
+        elif s[left].lower() in vowels and s[right] not in vowels:
+            while(s[right].lower() not in vowels):
+                right -= 1
+        
+        elif s[left].lower() not in vowels and s[right].lower() in vowels:
+            while(s[left].lower() not in vowels):
+                left += 1
+        
+        else:
+            left +=1 
+            right -= 1
+    return  "".join(list(lst_str))
+        
 
-        try:
-            img = Image.open(png_path)
 
-            # Convert RGBA images properly
-            img.save(webp_path, "WEBP", quality=80)
-
-            print(f"Converted: {filename} -> {webp_filename}")
-
-        except Exception as e:
-            print(f"Error converting {filename}: {e}")
-
-print("Conversion completed!")
+print(reverseVowels(s))
