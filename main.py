1722fa28dd0010c890f3ac4850c3d3db1b0a48db
Mridulsharma1133 <mridulsharma1133@gmail.com>
2026-06-05 13:07:46 +0530

    solved moving zeroes to end problem

--- main.py
+++ main.py
@@ -1,33 +1,13 @@
-s = "leetcode"
+nums = [0,1,0,3,12]
+res = [0] * len(nums)
 
-print(s[3].lower())
+i = 0
+for j in range(len(nums)):
+    if (nums[j] == 0):
+        continue
+    else:
+        res[i] = nums[j]
+        i += 1
 
-
-
-def reverseVowels(s):
-    lst_str = list(s)
-    left  = 0
-    vowels = "aeiou"
-    right = len(s) - 1
-    while(left < right):
-        if s[left].lower() in vowels and s[right].lower() in vowels:
-            lst_str[left], lst_str[right] = lst_str[right], lst_str[left]
-            left += 1
-            right -= 1
-        
-        elif s[left].lower() in vowels and s[right] not in vowels:
-            while(s[right].lower() not in vowels):
-                right -= 1
-        
-        elif s[left].lower() not in vowels and s[right].lower() in vowels:
-            while(s[left].lower() not in vowels):
-                left += 1
-        
-        else:
-            left +=1 
-            right -= 1
-    return  "".join(list(lst_str))
-        
-
-
-print(reverseVowels(s))
+nums[:] = res
+print(nums)
