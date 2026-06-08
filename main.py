8966d09a804f97fa34021090e1b903ed749a950d
Mridulsharma1133 <mridulsharma1133@gmail.com>
2026-06-08 11:53:39 +0530

    Finding the third maximum

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-nums = [0,1,0,3,12]
-res = [0] * len(nums)
+nums = [2,2,3,1]
+nums = set(nums)
+if len(nums) < 3: 
+    print(min(nums))
 
-i = 0
-for j in range(len(nums)):
-    if (nums[j] == 0):
-        continue
-    else:
-        res[i] = nums[j]
-        i += 1
-
-nums[:] = res
-print(nums)
+else:
+    nums.remove(max(nums))
+    nums.remove(max(nums))
+    print(max(nums))
